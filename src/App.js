592ecf8ed7809a8edf79fab423ea5a9ced310626
akwjr963 <akwjr963@daum.net>
2024-05-13 14:35:09 +0900

    chapter2 장보기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,121 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
 
-export default class App extends Component {
-    render() {
-        return (
-            <main className="main-container">
-                <div className="sub-container">
-                    <h1>장바구니</h1>
+const App = () => {
+    const [expenses, setExpenses] = useState([
+        { id: 1, charge: '콜라', amount: 2000 },
+        { id: 2, charge: '빵', amount: 1000 },
+        { id: 3, charge: '맥북', amount: 20000 },
+    ]);
 
-                    <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>{/*Expense Form */}Expense Form</div>
-                    <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>{/*Expense List */}Expense List</div>
-                    <div style={{ display: 'flex', justifyContent: 'start', marginTop: '1rem' }}>
-                        <p style={{ fontSize: '2rem' }}>총 합계: </p>
-                    </div>
+    const [charge, setCharge] = useState('');
+    const [amount, setAmount] = useState(0);
+    const [id, setId] = useState('');
+    const [edit, setEdit] = useState(false);
+
+    const [alert, setAlert] = useState({ show: false });
+
+    const handleEdit = (id) => {
+        const expense = expenses.find((item) => item.id === id);
+        const { charge, amount } = expense;
+        setCharge(charge);
+        setAmount(amount);
+        setId(id);
+        setEdit(true);
+    };
+
+    const handleCharge = (e) => {
+        setCharge(e.target.value);
+    };
+
+    const handleAmount = (e) => {
+        setAmount(e.target.valueAsNumber);
+    };
+
+    const handleDelete = (id) => {
+        const newExpense = expenses.filter((expense) => expense.id !== id);
+        setExpenses(newExpense);
+        handleAlert({ type: 'danger', text: '아이템이 삭제 되었습니다.' });
+    };
+
+    const clearItems = () => {
+        setExpenses([]);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (charge !== '' && amount > 0) {
+            if (edit) {
+                const newExpenses = expenses.map((item) => {
+                    return item.id === id ? { ...item, charge, amount } : item;
+                });
+                setExpenses(newExpenses);
+                setEdit(false);
+                handleAlert({ type: 'success', text: '아이템이 수정되었습니다.' });
+            } else {
+                const newExpense = { id: crypto.randomUUID(), charge, amount };
+                const newExpenses = [...expenses, newExpense];
+                setExpenses(newExpenses);
+                setCharge('');
+                setAmount(0);
+                handleAlert({ type: 'success', text: '아이템이 생성되었습니다.' });
+            }
+        } else {
+            handleAlert({ type: 'danger', text: 'charge는 빈 값일 수 없으며 amount 값은 0보다 커야 합니다.' });
+        }
+    };
+
+    const handleAlert = ({ type, text }) => {
+        setAlert({ show: true, type, text });
+        setTimeout(() => {
+            setAlert({ show: false });
+        }, 7000);
+    };
+
+    return (
+        <main className="main-container">
+            <div className="sub-container">
+                {alert.show ? <Alert type={alert.type} text={alert.text} /> : null}
+                <h1>장바구니</h1>
+
+                <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
+                    {/*Expense Form */}
+                    <ExpenseForm
+                        edit={edit}
+                        handleSubmit={handleSubmit}
+                        charge={charge}
+                        handleCharge={handleCharge}
+                        amount={amount}
+                        handleAmount={handleAmount}
+                    />
                 </div>
-            </main>
-        );
-    }
-}
+                <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
+                    {/*Expense List */}
+                    <ExpenseList
+                        expenses={expenses}
+                        clearItems={clearItems}
+                        handleEdit={handleEdit}
+                        initialExpenses={expenses}
+                        handleDelete={handleDelete}
+                    />
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'start', marginTop: '1rem' }}>
+                    <p style={{ fontSize: '2rem' }}>
+                        총 합계:{' '}
+                        <span>
+                            {expenses.reduce((acc, curr) => {
+                                return (acc += curr.amount);
+                            }, 0)}
+                            원
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default App;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import './ExpenseList.css';
+import { MdDelete } from 'react-icons/md';
+
+const ExpenseList = ({ initialExpenses, handleDelete, handleEdit, clearItems, expenses }) => {
+    return (
+        <>
+            <ul className="list">
+                {initialExpenses.map((expense) => {
+                    return <ExpenseItem key={expense.id} expense={expense} handleDelete={handleDelete} handleEdit={handleEdit} />;
+                })}
+            </ul>
+            {expenses.length > 0 ? (
+                <button className="btn" onClick={clearItems}>
+                    목록 지우기
+                    <MdDelete className="btn-icon" />
+                </button>
+            ) : null}
+        </>
+    );
+};
+
+export default ExpenseList;
